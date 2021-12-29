39b6b4001269c847f3afb8cd98aacf97eb73dd6f
David Manriquez <dmanriqu@my.chemeketa.edu>
2021-12-28 21:07:29 -0800

    recursion and conditional statements

--- app.js
+++ app.js
@@ -9,18 +9,6 @@ const promptUser = () => {
       type: 'input',
       name: 'name',
       message: 'What is your name?'
-    }
-  ]);
-};
-
-promptUser().then(answers => console.log(answers));
-
-const promptUser = () => {
-  return inquirer.prompt([
-    {
-      type: 'input',
-      name: 'name',
-      message: 'What is your name?'
     },
     {
       type: 'input',
@@ -34,6 +22,75 @@ const promptUser = () => {
     }
   ]);
 };
+
+
+
+
+
+
+const promptProject = portfolioData => {
+  console.log(`
+=================
+Add a New Project
+=================
+`);
+
+// If there's no 'projects' array property, create one
+if (!portfolioData.projects) {
+  portfolioData.projects = [];
+}
+
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the name of your project?'
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of the project (Required)'
+    },
+    {
+      type: 'checkbox',
+      name: 'languages',
+      message: 'What did you build this project with? (Check all that apply)',
+      choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+    },
+    {
+      type: 'input',
+      name: 'link',
+      message: 'Enter the GitHub link to your project. (Required)'
+    },
+    {
+      type: 'confirm',
+      name: 'feature',
+      message: 'Would you like to feature this project?',
+      default: false
+    },
+    {
+      type: 'confirm',
+      name: 'confirmAddProject',
+      message: 'Would you like to enter another project?',
+      default: false
+      
+    }
+  ])
+    .then(projectData => {
+      portfolioData.projects.push(projectData);
+      if (projectData.confirmAddProject) {
+        return promptProject(portfolioData);
+      } else {
+        return portfolioData;
+      }
+    });
+  };
+
+promptUser()
+.then(promptProject)
+.then(portfolioData => {
+  console.log(portfolioData);
+});
 //const profileDataArgs = process.argv.slice(2);
 // const pageHTML = generatePage(name, github);
 //   fs.writeFile('index.html', generatePage(name, github), err => {
