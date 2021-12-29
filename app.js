257575d72d466035bf6d67824148568dd01616e3
David Manriquez <dmanriqu@my.chemeketa.edu>
2021-12-28 20:37:16 -0800

    question input

--- app.js
+++ app.js
@@ -1,16 +1,42 @@
-const fs = require('fs');
-const generatePage = require ('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const name = profileDataArgs[0];
-const github = profileDataArgs[1];
+//const fs = require('fs');
+const inquirer = require('inquirer');
+console.log(inquirer)
+//const generatePage = require ('./src/page-template.js');
 
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ]);
+};
 
-  
-  
+promptUser().then(answers => console.log(answers));
 
-
-  fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
-  
-    console.log('Portfolio complete! Check out index.html to see the output!');
-  });
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username'
+    },
+    {
+      type: 'input',
+      name: 'about',
+      message: 'Provide some information about yourself:'
+    }
+  ]);
+};
+//const profileDataArgs = process.argv.slice(2);
+// const pageHTML = generatePage(name, github);
+//   fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw err;  
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+//   });
